01bb73411d2c9b37c6c3dc0bad14ca728b60fd9e
henil code <henilcode@gmail.com>
2023-02-19 20:03:23 +0530

    fixed bug in js

--- public/js/script.js
+++ public/js/script.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
                 $("#msger-chat").empty();
                 data.forEach((element) => {
                     $("#msger-chat").append(
-                        `<div class="msg ${element['users_id']==1?'left-msg':'right-msg'}"> <div class="msg-img shadow fw-bold" style="padding-top: 13px;padding-left:14px;"> HP </div> <div class="msg-bubble"> <div class="msg-info"> <div class="msg-info-name">${element['users_id']}</div> <div class="msg-info-time">${element['created_at']}</div> </div> <div class="msg-text"> ${element['chat']} </div> </div> </div>`
+                        `<div class="msg ${element['users_id']==users_id?'right-msg':'left-msg'}"> <div class="msg-img shadow fw-bold" style="padding-top: 13px;padding-left:14px;"> HP </div> <div class="msg-bubble"> <div class="msg-info"> <div class="msg-info-name">${element['users_id']}</div> <div class="msg-info-time">${element['created_at']}</div> </div> <div class="msg-text"> ${element['chat']} </div> </div> </div>`
                     );
                 });
                 console.log(data);
