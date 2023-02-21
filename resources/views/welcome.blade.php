2e38231db3badfec8301b2008f8cd0817d6c7d33
henil code <henilcode@gmail.com>
2023-02-21 09:43:56 +0530

    edit ui off welcome page and set create romm ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,44 +1,55 @@
 @extends('header')
 @section('body')
+    <style>
+        .box-center {
+            display: grid;
+            height: 70vh;
+            place-items: center;
+        }
+    </style>
     <div class="my-5"></div>
     <div class="container">
-        <div class="text-center">
-            <div>
-                <form action="{{ route('create-room') }}" method="post" class="row col-5 m-auto shadow g-3 "
-                    autocomplete="off">
-                    @csrf
-                    <div class="col-auto">
-                        <label for="room" class="col-form-label fw-bold"> Create Room : </label>
-                    </div>
-                    <div class="col-auto shadow">
-                        <input type="text" id="room" name="name" class="form-control">
-                    </div>
-                    <div class="col-auto shadow-lg">
-                        <input type="submit" value="Create Room" class="btn btn-primary">
-                    </div>
-                </form>
-                @error('name')
-                    <div class="alert alert-danger m-auto mt-5 w-25">
-                        {{ $message }}
-                    </div>
-                @enderror
+        <div class="row">
+            <div class="col-5">
+                <div class="box-center">
+                    <form autocomplete="off" action="{{ route('create-room') }}" method="post">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="create" class="form-label fw-bold fs-3">Create Room</label>
+                            <input type="text" class="form-control fs-5 mt-2" name="name" id="create"
+                                value="{{ old('name') }}" placeholder="type hear" aria-describedby="createRoom">
+                            @error('name')
+                                <div id="createRoom" class="form-text text-end text-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="mt-4">
+                            <button type="submit" class="btn btn-primary">Let's Create</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
-        <div class="text-center">
-            <div>
-                <form action="{{ route('join-room') }}" method="post" class="row col-5 m-auto shadow g-3 "
-                    autocomplete="off">
-                    @csrf
-                    <div class="col-auto">
-                        <label for="room" class="col-form-label fw-bold">Join Room : </label>
-                    </div>
-                    <div class="col-auto shadow">
-                        <input type="text" id="room" name="name" class="form-control">
-                    </div>
-                    <div class="col-auto shadow-lg">
-                        <input type="submit" value="Join Room" class="btn btn-primary">
-                    </div>
-                </form>
+            <div class="col-2">
+                <div class="box-center">
+                    <span class="border border-gray h-100"></span>
+                    <h1 class="text-muted">or</h1>
+                    <span class="border border-gray h-100"></span>
+                </div>
+            </div>
+            <div class="col-5">
+                <div class="box-center">
+                    <form autocomplete="off" action="{{ route('join-room') }}" method="post">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="join" class="form-label fw-bold fs-3">Join Room</label>
+                            <input type="text" class="form-control fs-5 mt-2" placeholder="type hear" id="join"
+                                name="name" aria-describedby="joinRoom">
+                            {{-- <div id="joinRoom" class="form-text text-end text-danger">We'll nevenyone else.</div> --}}
+                        </div>
+                        <div class="mt-4">
+                            <button type="submit" class="btn btn-primary">Let's Join</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
